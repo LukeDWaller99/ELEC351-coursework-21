639977e0b91a17f5c2979523edb14e47e113eb64
gnarvo <harvey.noah@yahoo.com>
2021-12-01 18:09:53 +0000

    make print queues v2

--- printQueue.cpp
+++ printQueue.cpp
@@ -2,3 +2,30 @@
 
 EventQueue printQueue;
 
+//**************************
+//BUFFER PRINT QUEUES
+//**************************
+void bufferFull(){
+    printf("CRITICAL ERROR - the buffer is full. you cannot write to the buffer. \n");
+}
+
+void bufferLockTimeout(){
+    printf("CRITICAL ERROR - mutex lock timeout, cannot acquire lock. \n");
+
+}
+
+void timeLockTimeout(){
+    printf("CRITICAL ERROR - time lock timeout. \n");
+}
+
+void emptyFlush(){
+    printf("NOT SO CRITICAL ERROR - worth mentioning that the buffer is empty \n");
+}
+
+void bufferFlushTimeout(){
+    printf("CRITIAL ERROR - buffer flush lock timeout, cannot acquire lock. \n");
+}
+
+void flushBuffer(){
+    printf("get flushed! data from buffer loaded onto sd card. \n"); 
+}
