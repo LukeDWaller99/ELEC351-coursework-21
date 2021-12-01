c2f3190ce0c97d375fd9666e971c91e5717f4135
jjpendlebury <61416766+jjpendlebury@users.noreply.github.com>
2021-12-01 12:05:19 +0000

    Update to main.cpp
    remove azure code

--- main.cpp
+++ main.cpp
@@ -12,58 +12,9 @@
 #include <string.h>
 using namespace uop_msb;
 
-extern void azureDemo();
-extern NetworkInterface *_defaultSystemNetwork;
 
-bool connect()
-{
-    LogInfo("Connecting to the network");
-
-    _defaultSystemNetwork = NetworkInterface::get_default_instance();
-    if (_defaultSystemNetwork == nullptr) {
-        LogError("No network interface found");
-        return false;
-    }
-
-    int ret = _defaultSystemNetwork->connect();
-    if (ret != 0) {
-        LogError("Connection error: %d", ret);
-        return false;
-    }
-    LogInfo("Connection success, MAC: %s", _defaultSystemNetwork->get_mac_address());
-    return true;
-}
-
-bool setTime()
-{
-    LogInfo("Getting time from the NTP server");
-
-    NTPClient ntp(_defaultSystemNetwork);
-    ntp.set_server("time.google.com", 123);
-    time_t timestamp = ntp.get_timestamp();
-    if (timestamp < 0) {
-        LogError("Failed to get the current time, error: %ud", timestamp);
-        return false;
-    }
-    LogInfo("Time: %s", ctime(&timestamp));
-    set_time(timestamp);
-    return true;
-}
 
 int main() {
-    if (!connect()) return -1;
-
-    if (!setTime()) return -1;
-
-    // The two lines below will demonstrate the features on the MSB. See uop_msb.cpp for examples of how to use different aspects of the MSB
-    // UOP_MSB_TEST board;  //Only uncomment for testing - DO NOT USE OTHERWISE
-    // board.test();        //Only uncomment for testing - DO NOT USE OTHERWISE
-
-    // Write fake data to Azure IoT Center. Don't forget to edit azure_cloud_credentials.h
-    printf("You will need your own connection string in azure_cloud_credentials.h\n");
-    LogInfo("Starting the Demo");
-    azureDemo();
-    LogInfo("The demo has ended");
-
-    return 0;
+    
+    // return 0;
 }
