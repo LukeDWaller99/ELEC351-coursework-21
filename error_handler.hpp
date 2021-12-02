6862f40a73b294458ccd44310fd0a95c3a7067cd
jjpendlebury <jackjonpendlebury@gmail.com>
2021-12-02 11:39:32 +0000

    Header and Class file creation

---
+++ error_handler.hpp
@@ -0,0 +1,20 @@
+#ifndef H_ERROR_HANDLER
+#define H_ERROR_HANDLER
+
+#define THREAD_NAME = eHandler;
+//error codes
+#define BUFFER_FULL = 10;
+#define BUFFER_LOCK_TIMEOUT = 11;
+#define TIMER_LOCK_TIMEOUT = 12;
+#define BUFFER_FLUSH_TIMEOUT = 13;
+
+
+class error_handler {
+    private:
+
+    public:
+
+}
+
+
+#endif
