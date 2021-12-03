46b89eba6c782c9da0656da2f6f96f7ddc4f5dcb
jjpendlebury <61416766+jjpendlebury@users.noreply.github.com>
2021-12-03 16:56:45 +0000

    Nearly complete class def

--- error_handler.cpp
+++ error_handler.cpp
@@ -1,1 +1,27 @@
 #include <error_handler.hpp>
+#include <uop_msb.h>
+
+void error_handler::clear_all(){
+    if (flagLock.trylock_for(10ms) == true){
+        ThisThread::flags_clear(0xFF);
+        flag_value=0;
+        flagLock.unlock();
+    }
+    else {
+        ERROR_THREAD_NAME.flags_set(0x99);
+    }
+}
+
+ void error_handler::error_thread(){
+    while(true){
+    ThisThread::flags_wait_any(0xFF); //wait until
+    flag_value = ThisThread::flags_get();
+    ThisThread::flags_clear(1);
+    }
+}
+
+error_handler::error_handler(){
+    Thread ERROR_THREAD_NAME;
+    ERROR_THREAD_NAME.set_priority(osPriorityRealtime);
+    ERROR_THREAD_NAME.start(error_thread);
+}
--- error_handler.hpp
+++ error_handler.hpp
@@ -1,20 +1,39 @@
 #ifndef H_ERROR_HANDLER
 #define H_ERROR_HANDLER
+#include "mbed.h"
+#include "Mutex.h"
 
-#define THREAD_NAME = eHandler;
+#define ERROR_THREAD_NAME eHandler
 //error codes
-#define BUFFER_FULL = 10;
-#define BUFFER_LOCK_TIMEOUT = 11;
-#define TIMER_LOCK_TIMEOUT = 12;
-#define BUFFER_FLUSH_TIMEOUT = 13;
+#define BUFFER_FULL = 10               //CRITICAL
+#define BUFFER_LOCK_TIMEOUT = 11       //CRITICAL
+#define TIMER_LOCK_TIMEOUT = 12        //CRITICAL
+#define EMPTY_FLUSH = 13               //WARNING
+#define BUFFER_FLUSH_TIMEOUT = 14      
+
+//sd card errors
+#define MOUNT_ERROR = 20        //checked if card mounted, it is not
+#define NO_SD_FILE = 21         //cannot find a new file to write in
+#define UNMOUNTED_FLUSH = 22    //you are trying to flush to an empty sd card slot
+
+//error handler errors
+#define FLAG_CLEAR_ERROR 99
+
 
 
 class error_handler {
     private:
+    Mutex flagLock;
+    static int flag_value;
+    void clear_all();
 
     public:
+    error_handler();
+    ~error_handler();
+    static void error_thread();
 
-}
+
+};
 
 
 #endif
