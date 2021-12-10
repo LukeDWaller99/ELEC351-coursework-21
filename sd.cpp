28dd79913f97ac216702f435d6c8dfc561096bba
gnarvo <harvey.noah@yahoo.com>
2021-12-10 14:32:33 +0000

    added lcd sd and printq

---
+++ sd.cpp
@@ -0,0 +1,72 @@
+#include "mbed.h"
+#include "sd.h"
+#include "buffer.h"
+
+bufferClass obj;
+bool SDState = 0;
+FILE *fp; //file pointer
+
+// //function to flash green LED before and after the SD card is being written to, LED alternates state during write process
+// void flashGreen() {		
+// 	//always leaves green on
+// 	greenLED = 0;
+// 	ThisThread::sleep_for(500);		//put this thread to sleep for 500ms
+// 	greenLED = 1;
+// }
+
+void SDCard::initSD(){
+    if (SDState == 0){ //the card is not mounted
+        //criticalError - not fatal while initialising
+        printQueue.call(mountError);
+        //print to LCD?
+    }else{
+        //the sd card is mounted
+        //greenLED = 1;
+        printQueue.call(mountedSD);
+        //print to lcd saying mounted?
+    }
+} //end initSD function
+
+void SDCard::writeSD(){
+
+    FATFileSystem fs("sd, &sd"); //create filing system
+    FILE *fp = fopen("/sd/test.csv", "a"); //open so you can write
+
+    if (fp == NULL){ //cannot open file - it does not exist
+        //criticalError
+        //errorSeverity(CRITICAL);
+        printQueue.call(noSDFile);
+    }
+
+    if(SDState == 0){
+        //sd card is unmounted
+        //criticalError
+        printQueue.call(unmountedFlush);
+    }else{
+        //obj::flushBuffer(FILE *fp); //pass flushBuffer function file pointer
+        flushBuffer(*fp);
+        //bufferClass::flushBuffer(FILE &fp); //flush buffer data
+
+    }
+    fclose(fp); //close file
+} //end writeSD function
+
+void SDCard::unmountSD(){
+    SDCard::writeSD();
+    SDState = 0;
+    printQueue.call(unmountedSD);
+    //display on LCD?
+} //end unmountSD function
+
+//thread for SD writing
+void SDCard::SDThread(){
+    initSD();
+    while(1){
+        if(SDState == 1){
+            //if buffer is almost full at 1 minute, flush
+
+            //if buffer hasn't filled in an hour, still flush
+
+        }
+    }
+} //end SDThread function
