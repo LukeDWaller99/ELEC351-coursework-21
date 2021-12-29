e59041f8472f97a54152998aa35f3cfd627f6892
jjpendlebury <jackjonpendlebury@gmail.com>
2021-12-29 17:34:51 +0000

    Funky Buzzer Noises

--- main.cpp
+++ main.cpp
@@ -20,17 +20,27 @@ SevenSegmentDisplay display;
 EnvSensor sensor;
 AnalogIn LDR(AN_LDR_PIN);
 Buzzer buzz;
-char note = D;
-char* notename;
+char note = C;
+char note2 = D;
+char note3 = E;
+char* notename,notename2,notename3;
 
 
 
 int main() {
     notename = &note;
+    notename2 = note2;
+    notename3 = note3;
     display.test();
     matrix.test();
     buzz.playTone(notename);
-    wait_us(100000);
+    wait_us(1000000);
+    buzz.rest();
+    buzz.playTone(&note2,uop_msb::Buzzer::HIGHER_OCTAVE);
+    wait_us(1000000);
+    buzz.rest();
+    buzz.playTone(&note3,uop_msb::Buzzer::LOWER_OCTAVE);
+    wait_us(1000000);
     buzz.rest();
     while (true){
     wait_us(1000000);
