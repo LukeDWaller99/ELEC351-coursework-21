f01d7c7a78311040e64dd3d41e1db593cf964335
jjpendlebury <jack.j.pendlebury@students.plymouth.ac.uk>
2022-01-09 13:20:20 +0000

    Removal of Azure

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include "uop_msb.h"
 #include "rtos/ThisThread.h"
 #include "NTPClient.h"
-#include "azure_c_shared_utility/xlogging.h"
 #include <cstring>
 #include <string.h>
 #include <LEDMatrix.h>
@@ -22,56 +21,8 @@ Thread t;
 
 
 int main() {
-    matrix.clear();
-    display.clear();
-    display.test();
     while(true)
     {
-        // samples[0] = 0xffff;
-        // samples[1] = 0x0000;
-        // samples[2] = 0xff00;
-        // samples[3] = 0xf0f0;
-        // samples[4] = 0x0f0f;
-        // samples[5] = 0x000f;
-        // samples[6] = 0xfff0;
-        // samples[7] = 0x0ff0;
-
-    // if (!connect()) return -1;
-
-    // if (!setTime()) return -1;
-
-    // // The two lines below will demonstrate the features on the MSB. See uop_msb.cpp for examples of how to use different aspects of the MSB
-    // // UOP_MSB_TEST board;  //Only uncomment for testing - DO NOT USE OTHERWISE
-    // // board.test();        //Only uncomment for testing - DO NOT USE OTHERWISE
-
-    // // Write fae data to Azure IoT Center. Don't forget to edit azure_cloud_credentials.h
-    // printf("You will need your own connection string in azure_cloud_credentials.h\n");
-    // LogInfo("Starting the Demo");
-    // azureDemo();
-    // LogInfo("The demo has ended");
-
-    // return 0;
-
-    // matrix.clear();
-
-    // Err_thread.start(&EH.error_thread);
-    // display.test();
-    // matrix.test();
-    t.start(callback(queue, &EventQueue::dispatch_forever));
-    matrix.clear();
-    wait_us(100000);
-    EH.setErrorFlag(T_UPPER);
-    wait_us(5000000);
-    EH.setErrorFlag(ALL_CLEAR);
-    wait_us(100000);
-    EH.setErrorFlag(EMPTY_FLUSH);
-    wait_us(5000000);
-    EH.setErrorFlag(ALL_CLEAR);
-    wait_us(5000000);
-    EH.setErrorFlag(BUFFER_FULL);
-         while(true)
-     {
         
-     }
-    //EH.alarmtest();
+    }
 }
