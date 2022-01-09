cafc12fe6348ca6fa12b9d7f2e57c11c626f457a
jjpendlebury <jack.j.pendlebury@students.plymouth.ac.uk>
2022-01-09 14:42:55 +0000

    Updated documentation

--- CustomQueue.h
+++ CustomQueue.h
@@ -1,3 +1,12 @@
+/**
+@file CustomQueue.h
+CustomQueue class header file. This class contains an event queue with a wrapper.
+This wrapper ensures that the queue's thread is initialised and that the queue is
+dispatched before being used by any of the other classes. Several of the classes
+have status prints within their constructors, so a pre-main initialisation is critical
+to capture all status messages.
+**/
+
 #ifndef __CUSTOM_QUEUE__
 #define __CUSTOM_QUEUE__
 
@@ -5,14 +14,23 @@
 #include "uop_msb.h"
 #include "string.h"
 
+
+/**
+Event Queue wrapper class.
+**/
 class CustomQueue{
     private:
 
     public: 
+    /**
+    Default constructor. This constructor initialises the Thread Object
+    QUEUE_THREAD and sets the queue to dispatch forever via a callback. 
+    The message @code "Queue Initialised" @endcode will be displayed once 
+    initialisation has been completed.
+    **/
     CustomQueue();
-    EventQueue queue;
-    Thread QUEUE_THREAD;
-    void call(string str);
+    EventQueue queue;       ///<Event Queue Object. This class is a wrapper for this object.
+    Thread QUEUE_THREAD;    ///<Thread Object. This thread is solely responsible for the queue.
 };
 
 #endif
